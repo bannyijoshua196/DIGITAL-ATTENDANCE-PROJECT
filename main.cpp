70905756e42492848bca4ec1161d2f37c3a3f708
Jeremiah Dornu <jeremiahdornu70@gmail.com>
2026-03-02 17:02:27 +0000

    Implemented student registration and viewing

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 using namespace std;
 
@@ -7,13 +8,35 @@ public:
     string indexNumber;
     string name;
     string program;
-
-    void display() {
-        cout << indexNumber << " " << name << " " << program << endl;
-    }
 };
 
+void addStudent() {
+    ofstream file("students.txt", ios::app);
+    Student s;
+
+    cout << "Index Number: ";
+    cin >> s.indexNumber;
+    cin.ignore();
+    cout << "Name: ";
+    getline(cin, s.name);
+    cout << "Program: ";
+    getline(cin, s.program);
+
+    file << s.indexNumber << "," << s.name << "," << s.program << endl;
+    file.close();
+}
+
+void viewStudents() {
+    ifstream file("students.txt");
+    string line;
+    while (getline(file, line)) {
+        cout << line << endl;
+    }
+    file.close();
+}
+
 int main() {
-    cout << "Digital Attendance System\n";
+    addStudent();
+    viewStudents();
     return 0;
 }
